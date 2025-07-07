94b8ee3e7dc14befb5c5c4d3215bc1d52adce32b
arshiasharma08 <84686827+arshiasharma08@users.noreply.github.com>
2025-07-06 19:57:02 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -23,26 +23,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 choice1 = input('You\'re at a crossroad, where do you want to go? '
-      'Type "left" or "right". ').lower()
+                'Type "left" or "right".\n').lower()
 
 if choice1 == "left":
-    choice2 = input('You\'ve come to a lake, '
-          'there is an island in the middle of the lake. '
-          'Type "wait" to wait for a boat. '
-          'Type "swim" to swim across. ').lower()
+    choice2 = input('You\'ve come to a lake. '
+                    'There is an island in the middle of the lake. '
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.\n').lower()
     if choice2 == "wait":
         choice3 = input("You arrive at the island unharmed. "
-                        "There is a house with 3 doors. "
-                        "One red, one yellow and one blue. "
-                        "Which color do you choose? ").lower()
+                        "There is house with 3 doors. One red, "
+                        "one yellow and one blue. "
+                        "Which colour do you choose?\n").lower()
         if choice3 == "red":
-            print("It's a room full of fire. Game Over.")
+            print("It's a room full of fire. Game Over")
         elif choice3 == "yellow":
             print("You found the treasure. You Win!")
         elif choice3 == "blue":
-            print("You enter a room full of beasts. Game Over")
+            print("You enter a room of beasts. Game Over.")
         else:
-            print("You chose a door that doesn't exist. Game Over")
+            print("You chose a door that doesn't exist. Game Over.")
     else:
         print("You got attacked by an angry trout. Game Over.")
 
--- task.md
+++ task.md
@@ -1,15 +1,17 @@
-We're going to build a tip calculator.
+Your goal today is to build a "Chose your own adventure game".
+Using what you have learnt in the lessons today you will be building a very simple version of this type of text game. 
 
-If the bill was $150.00, split between 5 people, with 12% tip. 
+Use the flow chart [linked here](https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload) to create the game logic. 
 
-Each person should pay:
+Once you've completed the project, you can always extend the game and make it more interesting!
 
-`(150.00 / 5) * 1.12 = 33.6`
+<div class="hint">
+  You can use the lower() function to turn any string into all lower case. 
+https://www.w3schools.com/python/ref_string_lower.asp
 
-After formatting the result to 2 decimal places = `33.60`
+Alternatively you can also use the logical operator "or" to check for other spellings of user choice. e.g. Right, right or RIGHT.
+</div>
 
-## Demo
-https://appbrewery.github.io/python-day2-demo/
 
-### Very Optional Reading: Floating Point Arithmetic
-https://docs.python.org/3/tutorial/floatingpoint.html
+### Demo
+https://appbrewery.github.io/python-day3-demo/
